599a9a1eb7f23704a60eab9037719e31e3dc6f3d
GabrielDeMiranda <gm851@live.mdx.ac.uk>
2021-02-14 10:36:40 +0000

    changing uri2

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports = {
     devServer: {
         proxy: {
             '/api': {
-                target: 'http://localhost:5000'
+                target: 'https://mevncw2.herokuapp.com'
             }
         }
     }
